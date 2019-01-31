afe039a5c718dfa89817bd2f01185218f263c68d
H3x <14954889+H3xFiles@users.noreply.github.com>
2019-01-31 19:09:45 +0100

    Update pointer_madness.c

--- pointers/pointer_madness.c
+++ pointers/pointer_madness.c
@@ -6,12 +6,15 @@ int main() {
     int *b = &a;
     int **c = &b;
     int d = *b;
-    int **e = d;
+    int **e = &d;
     int *f = b;
-    printf("a %p | %d\n",a, a);
-    printf("b %p | %d\n",b, *b);
-    printf("c %p | %d\n",c, **c);
-    printf("d %p | %d\n",d, d);
-    printf("e %p | %d\n",e, e);
-    printf("e %p | %d\n",f, f);
+    int *g = f;
+    
+    printf("&%p a point to %p | %d\n",&a,a, a);
+    printf("&%p b point to %p | %d\n",&b,b, *b);
+    printf("&%p c point to %p | %d\n",&c,c, **c);
+    printf("&%p d point to %p | %d\n",&d,d, d);
+    printf("&%p e point to %p | %d\n",&e,e, e);
+    printf("&%p f point to %p | %d\n",&f,f, f);
+    printf("&%p g point to %p | %d\n",&g,g, g);
 }
